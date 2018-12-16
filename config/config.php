29e094a95addb7234d602e9dc83bf83be905c2ae
Tushar402 <tushar.swe@gmail.com>
2018-12-16 22:42:29 +0600

    Add files via upload
    This is the configuration folder where the database is connected to the localhost.

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "handi_craft");
+define("TITLE", "Handi Craft");
+define("KEYWORDS", "Ecommerce, Online Shop, Shop BD");
